ee8eb854f77ec61b48a187f215dc08918d2408c7
xSAPSANx <misterdinal2000@mail.ru>
2024-08-07 18:45:33 +0300

    bag fix

--- src/index.js
+++ src/index.js
@@ -38,6 +38,13 @@ class Game extends React.Component {
 		})
 	}
 
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: step % 2 === 0,
+		})
+	}
+
 	render() {
 		const history = this.state.history
 		const current = history[this.state.stepNumber]
